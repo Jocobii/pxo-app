88818b22cce60b9e959d55507f20a07b89481ed9
jocobi <adalbertojocobi@gmail.com>
2022-10-21 08:29:15 -0700

    feat(app): configurate husky with eslint for commits and force style code

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:react-hooks/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:import/recommended',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'jsx-a11y', 'import', 'react-hooks'],
+    rules: {
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'linebreak-style': 0,
+        semi: 2,
+        indent: ['error', 4],
+    },
+};
