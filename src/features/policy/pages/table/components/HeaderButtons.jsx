1cb92c0f9ffe34e69a0fd253009c6a14e74c2d91
jocobi <adalbertojocobi@gmail.com>
2022-11-22 14:13:30 -0800

    feat(policy,quote): add correct name buttons filters

--- src/features/policy/pages/table/components/HeaderButtons.jsx
+++ src/features/policy/pages/table/components/HeaderButtons.jsx
@@ -1,27 +1,51 @@
 import React from 'react';
-import { Row, Button, Col } from 'antd';
+import {
+    Row, Button, Col, Input, Select,
+} from 'antd';
 import { useNavigate } from 'react-router-dom';
 
-const HeaderButtons = () => {
-    const navegate = useNavigate();
+const searchFields = [
+    { id: 'email', text: 'Distribuidor' },
+    { id: 'first_name', text: 'Nombre completo' },
+    { id: 'first_last_name', text: 'Telefono' },
+    { id: 'second_last_name', text: 'VIN' },
+];
 
+const HeaderButtons = () => {
+    const navigate = useNavigate();
     return (
-        <Row justify="space-between" style={{ marginBottom: '1rem' }}>
-            <Col>
-                <Button type="primary" style={{ marginRight: '1rem' }}>
-                    Filtro 1
-                </Button>
-                <Button type="primary" style={{ marginRight: '1rem' }}>
-                    Filtro 2
-                </Button>
-                <Button type="primary" style={{ marginRight: '1rem' }}>
-                    Filtro 3
-                </Button>
-            </Col>
-            <Col>
-                <Button type="primary" onClick={() => navegate('/policy/add')}>Agregar</Button>
-            </Col>
-        </Row>
+        (
+            <>
+                <Row
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row-reverse',
+                        marginBottom: '10px',
+                    }}
+                >
+                    <Input.Search
+                        style={{ width: '30%' }}
+                        addonBefore={(
+                            <Select
+                                defaultValue="second_last_name"
+                            >
+                                {searchFields.map((e) => (
+                                    <Select.Option key={e.id} value={e.id}>
+                                        {e.text}
+                                    </Select.Option>
+                                ))}
+                            </Select>
+                        )}
+                        placeholder="Buscar..."
+                    />
+                </Row>
+                <Row justify="space-between" style={{ marginBottom: '1rem', flexDirection: 'row-reverse' }}>
+                    <Col>
+                        <Button type="primary" onClick={() => navigate('/policy/add')}>Agregar</Button>
+                    </Col>
+                </Row>
+            </>
+        )
     );
 };
 
